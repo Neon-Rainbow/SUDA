c86d5744cdd9f29466733ede4a4bf12fea977c7d
水告木南 <Haonan_Fang@outlook.com>
2023-11-18 16:21:12 +0800

    Add comments for some algorithm code

--- Algorithm_Design/vjudge_problems/homework3/cpp/main.cpp
+++ Algorithm_Design/vjudge_problems/homework3/cpp/main.cpp
@@ -3,6 +3,20 @@
 
 // https://codeforces.com/contest/1676/problem/H2
 
+//该函数用于计算两个有序数组的逆序对数目
+//
+//首先,a[left...mid]和a[mid+1...right]都是有序的
+//然后,我们用两个指针i和j分别指向这两个数组的首元素
+//如果a[i]<a[j],那么a[i]就不会和a[j]构成逆序对
+//如果a[i]>=a[j],那么a[i]就会和a[j...mid]构成逆序对
+//因为a[j...mid]都是大于等于a[j]的,所以a[i]也会大于等于a[j]
+//所以a[i]也会大于等于a[j...mid]中的每一个元素,所以会构成逆序对
+//
+//@param a: 待计算的数组
+//@param left: 数组的左边界
+//@param mid: 数组的中间位置
+//@param right: 数组的右边界
+//@return: 逆序对的数目
 long long merge(std::vector<int> &a, int left, int mid, int right) {
     int i = left, j = mid + 1, k = 0;
     std::vector<int> temp(right - left + 1);
@@ -21,6 +35,13 @@ long long merge(std::vector<int> &a, int left, int mid, int right) {
     return count;
 }
 
+/* 该函数用来计算数组a[left...right]中的逆序对数目
+ *
+ * @param a: 待计算的数组
+ * @param left: 数组的左边界
+ * @param right: 数组的右边界
+ * @return: 逆序对的数目
+ */
 long long countInversions(std::vector<int> &a, int left, int right) {
     long long count = 0;
     if (left < right) {
