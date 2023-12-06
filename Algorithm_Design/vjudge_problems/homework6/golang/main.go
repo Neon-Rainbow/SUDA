0b5ce4a0afec552d5c8d8fa390fc2bfddca640bc
水告木南 <Haonan_Fang@outlook.com>
2023-12-06 16:09:35 +0800

    Refactor bitset initialization and manipulation

--- Algorithm_Design/vjudge_problems/homework6/golang/main.go
+++ Algorithm_Design/vjudge_problems/homework6/golang/main.go
@@ -7,38 +7,61 @@ import (
 	"strconv"
 )
 
-const MaxLength = 200001
-
 func main() {
+	// 初始化缓冲读取器和写入器
 	scanner := bufio.NewScanner(os.Stdin)
 	writer := bufio.NewWriter(os.Stdout)
 	defer writer.Flush()
 
 	scanner.Split(bufio.ScanWords)
 
+	// nextInt 是一个辅助函数，用于从输入中读取下一个整数
 	nextInt := func() int {
 		scanner.Scan()
 		val, _ := strconv.Atoi(scanner.Text())
 		return val
 	}
 
+	// 读取测试用例的数量
 	t := nextInt()
 	for ; t > 0; t-- {
+		// 读取 n 的值
 		n := nextInt()
-		btst := make([]bool, MaxLength)
-		btst[0] = true
+		// 动态确定位集合的大小
+		bitsetSize := (n + 64) / 64
+		// 初始化位集合
+		btst := make([]uint64, bitsetSize)
 
+		// setBit 函数用于在 btst 中设置位
+		setBit := func(pos int) {
+			btst[pos/64] |= 1 << (pos % 64)
+		}
+
+		// getBit 函数用于获取 btst 中某一位的状态
+		getBit := func(pos int) bool {
+			return btst[pos/64]&(1<<(pos%64)) != 0
+		}
+
+		// 设置起始位
+		setBit(0)
+
+		// 主逻辑循环
 		for i := 1; i <= n; i++ {
 			x := nextInt()
-			if i+x < MaxLength {
-				btst[i+x] = btst[i-1] || btst[i+x]
+			if i+x < n+1 {
+				if getBit(i - 1) {
+					setBit(i + x)
+				}
 			}
 			if i-x-1 >= 0 {
-				btst[i] = btst[i-x-1] || btst[i]
+				if getBit(i - x - 1) {
+					setBit(i)
+				}
 			}
 		}
 
-		if btst[n] {
+		// 根据位集合的最终状态输出结果
+		if getBit(n) {
 			fmt.Fprintln(writer, "YES")
 		} else {
 			fmt.Fprintln(writer, "NO")
