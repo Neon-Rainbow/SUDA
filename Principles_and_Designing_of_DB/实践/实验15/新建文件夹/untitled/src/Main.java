23e6d4ed313c3668f9cde748be39a94d4b8d113e
水告木南 <Haonan_Fang@outlook.com>
2023-12-21 10:36:15 +0800

    Delete unnecessary files and folders

--- Principles_and_Designing_of_DB/实践/实验15/新建文件夹/untitled/src/Main.java
+++
@@ -1,9 +0,0 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
